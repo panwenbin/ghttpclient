7f2ce8e5a33a3df44c3edc58c2868ff703a6c52b
Pan Wenbin <panwenbin@gmail.com>
2019-06-29 19:54:42 +0800

    add tests for simple client

---
+++ simple_client_test.go
@@ -0,0 +1,89 @@
+// Copyright 2019 潘文斌. All rights reserved.
+// license that can be found in the LICENSE file.
+
+package ghttpclient_test
+
+import (
+	"github.com/panwenbin/ghttpclient"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	response, err := ghttpclient.Get("http://cp.fei.lv/ghttpclient", nil)
+	if err != nil {
+		t.Error("network error")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if strings.Compare("ghttpclient", string(body)) != 0 {
+		t.Errorf("expect 'ghttpclient, got %s", body)
+	}
+}
+
+func TestPost(t *testing.T) {
+	response, err := ghttpclient.Post("http://cp.fei.lv/", strings.NewReader("ghttpclient"), nil)
+	if err != nil {
+		t.Error("network error")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if strings.Compare("ghttpclient", string(body)) != 0 {
+		t.Errorf("expect 'ghttpclient, got %s", body)
+	}
+}
+
+func TestPut(t *testing.T) {
+	response, err := ghttpclient.Put("http://cp.fei.lv/put", strings.NewReader("ghttpclient"), nil)
+	if err != nil {
+		t.Error("network error")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if strings.Compare("ghttpclient", string(body)) != 0 {
+		t.Errorf("expect 'ghttpclient, got %s", body)
+	}
+}
+
+func TestPatch(t *testing.T) {
+	response, err := ghttpclient.Patch("http://cp.fei.lv/patch", strings.NewReader("ghttpclient"), nil)
+	if err != nil {
+		t.Error("network error")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if strings.Compare("ghttpclient", string(body)) != 0 {
+		t.Errorf("expect 'ghttpclient, got %s", body)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	response, err := ghttpclient.Delete("http://cp.fei.lv/ghttpclient", nil)
+	if err != nil {
+		t.Error("network error")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if strings.Compare("ghttpclient", string(body)) != 0 {
+		t.Errorf("expect 'ghttpclient, got %s", body)
+	}
+}
+
+func TestOptions(t *testing.T) {
+	response, err := ghttpclient.Options("http://cp.fei.lv/ghttpclient", nil)
+	if err != nil {
+		t.Error("network error")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if strings.Compare("ghttpclient", string(body)) != 0 {
+		t.Errorf("expect 'ghttpclient, got %s", body)
+	}
+}
