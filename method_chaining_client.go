52f816d88783e2cfce760042c1a4620e3041f5e3
Pan Wenbin <panwenbin@gmail.com>
2020-02-24 10:39:00 +0800

    transport uses same parameters as DefaultTransport

--- method_chaining_client.go
+++ method_chaining_client.go
@@ -10,6 +10,7 @@ import (
 	"github.com/panwenbin/ghttpclient/header"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -172,6 +173,19 @@ func (g *GHttpClient) prepare(method string) error {
 
 	g.client = &http.Client{}
 
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
 	if g.cookieJar != nil {
 		g.client.Jar = g.cookieJar
 	}
@@ -181,10 +195,10 @@ func (g *GHttpClient) prepare(method string) error {
 	}
 
 	if g.sslSkipVerify {
-		g.client.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
+
+	g.client.Transport = transport
 
 	return nil
 }
